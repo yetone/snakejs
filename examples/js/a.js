96be626555a9d3b96e8c323243d41868c8be2ed5
yetone <i@yetone.net>
2014-10-22 18:34:58 +0800

    fix

--- examples/js/a.js
+++ examples/js/a.js
@@ -1,6 +1,6 @@
 snake.use('foo b', function(foo, b) {
-  console.log('*** in a.js ***')
+  console.log('*** in a.js ***');
   foo.bar();
   b.c();
-  console.log('*** out a.js ***')
+  console.log('*** out a.js ***');
 });

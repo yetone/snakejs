36665ea58dd9362c44ca80dcacc59d904164989c
yetone <i@yetone.net>
2014-10-22 18:40:47 +0800

    up

--- examples/js/b.js
+++ examples/js/b.js
@@ -1,7 +1,8 @@
-snake.define('b', function(require, exports, module) {
+snake.define('b', ['c'], function(require, exports, module, c) {
   module.exports = {
     c: function() {
       console.log('b.c');
     }
   };
+  c.d();
 });
